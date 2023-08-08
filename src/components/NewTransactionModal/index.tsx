8dc9b096fc0ad0e5f0b3c2c500ff8a5a0d5e70e9
Elioenay <elionay407@gmail.com>
2023-08-08 14:26:15 -0300

    Configurando JSON Server
    Realizando requisição HTTP
    
    Criando contexto de transações
    
    Calculando resumo de transações
    
    Formatando valores
    
    Criando hook para o resumo
    
    Aplicando React Hook Form
    
    Controlled components
    
    Buscando transações
    
    Configurando axios
    
    Criando nova transação
    
    Corrigindo erros de linting

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -8,7 +8,41 @@ import {
 } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
+import * as z from 'zod'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(['income', 'outcome']),
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
+
 export default function NewTransactionModal() {
+  const { createTransaction } = useContext(TransactionsContext)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+    control,
+    reset,
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+    defaultValues: { type: 'income' },
+  })
+
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    createTransaction(data)
+
+    reset()
+  }
+
   return (
     <Dialog.Portal>
       <Overlay />
@@ -19,41 +53,60 @@ export default function NewTransactionModal() {
           <X size={24} />
         </CloseButton>
 
-        <form action=''>
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
           <input
             type='text'
+            {...register('description')}
             required
             placeholder='Descrição'
           />
           <input
-            type='text'
+            type='number'
             required
             placeholder='Preço'
+            {...register('price', { valueAsNumber: true })}
           />
           <input
             type='text'
             required
             placeholder='Categoria'
+            {...register('category')}
           />
 
-          <TransactionType>
-            <TransactionTypeButton
-              value='income'
-              variant='income'
-            >
-              <ArrowCircleUp size={24} />
-              Entrada
-            </TransactionTypeButton>
-            <TransactionTypeButton
-              variant='outcome'
-              value='outcome'
-            >
-              <ArrowCircleDown size={24} />
-              Saída
-            </TransactionTypeButton>
-          </TransactionType>
+          <Controller
+            control={control}
+            name='type'
+            render={({ field }) => {
+              return (
+                <TransactionType
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <TransactionTypeButton
+                    value='income'
+                    variant='income'
+                  >
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
+                  <TransactionTypeButton
+                    variant='outcome'
+                    value='outcome'
+                  >
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionType>
+              )
+            }}
+          />
 
-          <button type='submit'>Cadastrar</button>
+          <button
+            disabled={isSubmitting}
+            type='submit'
+          >
+            Cadastrar
+          </button>
         </form>
       </Content>
     </Dialog.Portal>
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import Header from '../../components/Header'
 import Summary from '../../components/Summary'
 import SearchForm from './components/SearchForm'
@@ -6,8 +7,12 @@ import {
   TransactionsContainer,
   TransactionsTable,
 } from './styles'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 
 export default function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <div>
       <Header />
@@ -16,22 +21,25 @@ export default function Transactions() {
       <TransactionsContainer>
         <SearchForm />
         <TransactionsTable>
-          <tr>
-            <td width='50%'>Desenvolvimento de site</td>
-            <td>
-              <PriceHighLight variant='income'>R$ 12.000,00</PriceHighLight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-          <tr>
-            <td width='50%'>Desenvolvimento de site</td>
-            <td>
-              <PriceHighLight variant='outcome'>- R$ 12.000,00</PriceHighLight>
-            </td>
-            <td>Alimentação</td>
-            <td>12/04/2022</td>
-          </tr>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
         </TransactionsTable>
       </TransactionsContainer>
     </div>
