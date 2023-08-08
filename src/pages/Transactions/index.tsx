43d2209ac8a7f71e1456093f979c1881789dcfef
Elioenay <elionay407@gmail.com>
2023-08-08 10:49:15 -0300

    Componente: Summary
    Componente: SearchForm
    
    Modal de nova transação
    
    Botões de entrada e saída
    
    Criando radio button acessível

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,39 @@
 import Header from '../../components/Header'
+import Summary from '../../components/Summary'
+import SearchForm from './components/SearchForm'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export default function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tr>
+            <td width='50%'>Desenvolvimento de site</td>
+            <td>
+              <PriceHighLight variant='income'>R$ 12.000,00</PriceHighLight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+          <tr>
+            <td width='50%'>Desenvolvimento de site</td>
+            <td>
+              <PriceHighLight variant='outcome'>- R$ 12.000,00</PriceHighLight>
+            </td>
+            <td>Alimentação</td>
+            <td>12/04/2022</td>
+          </tr>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
