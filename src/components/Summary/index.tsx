78f35a515c670550d3ff6d4ebb0c0537e8b839fe
Elioenay <elionay407@gmail.com>
2023-08-08 15:18:05 -0300

    Calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,25 @@ import { useContext } from 'react'
 export default function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -16,7 +35,7 @@ export default function Summary() {
             color='#00b37e'
           />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -26,7 +45,7 @@ export default function Summary() {
             color='#f75a68'
           />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
       <SummaryCard variant='green'>
         <header>
@@ -36,7 +55,7 @@ export default function Summary() {
             color='#fff'
           />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
