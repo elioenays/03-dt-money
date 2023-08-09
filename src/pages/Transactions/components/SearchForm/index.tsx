145b1e53610f44f0f30154fe6768a53d5b694a19
Elioenay <elionay407@gmail.com>
2023-08-08 21:57:21 -0300

    Aplicando Context Selectors

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -4,15 +4,20 @@ import { useForm } from 'react-hook-form'
 
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
 
 const searchFormSchema = z.object({ query: z.string() })
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export default function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    ({ fetchTransactions }) => {
+      return fetchTransactions
+    },
+  )
 
   const {
     register,
